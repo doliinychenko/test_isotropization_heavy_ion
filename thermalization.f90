ec21af0535418570228dd3aa4519eeb389c6d4aa
Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>
2014-12-23 14:58:07 +0100

    Printing out u, added jmu; real to double precision
    Also added total Tmn as 0th sort
    
    Signed-off-by: Dmytro Oliinychenko <oliiny@fias.uni-frankfurt.de>

--- thermalization.f90
+++ thermalization.f90
@@ -1,25 +1,26 @@
 program thermalization
  implicit none
 
- real, parameter :: pi = acos(-1.0)
- real dt, dx, dz
- real gs_sigma, many_sigma_sqr, gauss_denom, gauss_norm
+ double precision, parameter :: pi = dacos(-1.d0)
+ double precision dt, dx, dz
+ double precision gs_sigma, many_sigma_sqr, gauss_denom, gauss_norm
  integer nt, nx, nz
  integer max_sort
 
- real, dimension(:,:,:,:,:,:), allocatable :: Tmn, TmnL ! mu, nu, sort, t,x,z
- real, dimension(:,:,:,:), allocatable :: jBmu, jSmu ! mu, t,x,z
- real, dimension(:,:,:,:,:), allocatable :: umu ! mu, sort, t,x,z
- real, dimension(:,:), allocatable :: total_p ! mu, t
+ double precision, dimension(:,:,:,:,:,:), allocatable :: Tmn, TmnL ! mu, nu, sort, t,x,z
+ double precision, dimension(:,:,:,:,:), allocatable :: jmu ! mu, sort, t,x,z
+ double precision, dimension(:,:,:,:), allocatable :: jBmu, jSmu ! mu, t,x,z
+ double precision, dimension(:,:,:,:,:), allocatable :: umu ! mu, sort, t,x,z
+ double precision, dimension(:,:), allocatable :: total_p ! mu, t
  integer, dimension(:), allocatable :: total_B, total_S !t
 
  max_sort = 7
- dx = 1.0
- dz = 1.0
+ dx = 1.d0
+ dz = 1.d0
  nt = 10
  nx = 10
  nz = 10
- gs_sigma = 1.0
+ gs_sigma = 1.d0
  many_sigma_sqr = 4 * 4 * gs_sigma * gs_sigma
  gauss_denom = 2 * gs_sigma * gs_sigma
  gauss_norm = (2 * pi * gs_sigma * gs_sigma)**(-3./2.)
@@ -27,33 +28,67 @@ program thermalization
  call init_arrays()
  call Tmn_from_f14('/scratch/hyihp/oliiny/UrQMD_check/urqmd-3.4/test.f14')
  call print_conserved('conserved_quantities.txt')
- call print_Tmn('Tmn.txt', .FALSE., 0, 0) ! TRUE - Landau RF, FALSE - comp. frame
+ call print_Tmn('Tmn.txt', .False., 4, 0) ! FALSE - comp. frame
+ call get_Landau_Tmn()
+ call print_Tmn('TmnL.txt', .True., 4, 0) ! TRUE - Landau RF
+ call print_collective_velocities('v_collective.txt', 4, 0)
+ call delete_arrays_from_memory()
 
 contains
 
 subroutine init_arrays()
-  allocate(Tmn(0:3, 0:3, 1:max_sort,  1:nt, 0:nx, 0:nz))
-  allocate(TmnL(0:3, 0:3, 1:max_sort,  1:nt, 0:nx, 0:nz))
+  allocate(Tmn(0:3, 0:3, 0:max_sort,  1:nt, 0:nx, 0:nz))
+  allocate(TmnL(0:3, 0:3, 0:max_sort,  1:nt, 0:nx, 0:nz))
+  allocate(jmu(0:3, 0:max_sort,  1:nt, 0:nx, 0:nz))
   allocate(jBmu(0:3,  1:nt, 0:nx, 0:nz))
   allocate(jSmu(0:3,  1:nt, 0:nx, 0:nz))
-  allocate(umu(0:3, 1:max_sort,  1:nt, 0:nx, 0:nz))
+  allocate(umu(0:3, 0:max_sort,  1:nt, 0:nx, 0:nz))
   allocate(total_p(0:3, 1:nt))
   allocate(total_B(1:nt))
   allocate(total_S(1:nt))
-  Tmn     = 0.0
-  TmnL    = 0.0
-  jBmu    = 0.0
-  jSmu    = 0.0
-  total_p(0:3,:) = 0.0
+  Tmn     = 0.d0
+  TmnL    = 0.d0
+  jBmu    = 0.d0
+  jSmu    = 0.d0
+  jmu     = 0.d0
+  total_p(0:3,:) = 0.d0
   total_B(:) = 0
   total_S(:) = 0
-  umu(0,:,:,:,:) = 1.0
-  umu(1:3,:,:,:,:) = 0.0
+  umu(0,:,:,:,:) = 1.d0
+  umu(1:3,:,:,:,:) = 0.d0
+end subroutine
+
+subroutine delete_arrays_from_memory()
+  deallocate(Tmn)
+  deallocate(TmnL)
+  deallocate(jmu)
+  deallocate(jBmu)
+  deallocate(jSmu)
+  deallocate(umu)
+  deallocate(total_p)
+  deallocate(total_B)
+  deallocate(total_S)
+end subroutine
+
+subroutine get_Landau_Tmn()
+ use Land_Eck, only: FindLandau
+ implicit none
+
+ integer it, sort, ix, iz
+ do it = 1, nt
+   do sort = 0, max_sort
+     do ix = 0, nx; do iz = 0, nz
+       call FindLandau( Tmn(0:3, 0:3, sort, it, ix, iz),&
+                       TmnL(0:3, 0:3, sort, it, ix, iz),&
+                       umu(0:3, sort, it, ix, iz))
+     end do; end do
+   end do
+ end do
 end subroutine
 
 subroutine Tmn_from_f14(fname)
  character(len=*), intent(in) :: fname
- real Elab, r(0:3), p(0:3), m, dr(1:3), sf, upart(0:3)
+ double precision Elab, r(0:3), p(0:3), m, dr(1:3), sf, upart(0:3)
  integer tsteps, ev, Npart, i, nu, ityp, i3, sort, Bpart, Spart
  integer it, ix, iz
 
@@ -65,11 +100,12 @@ subroutine Tmn_from_f14(fname)
    print *, "Event number: ", ev, " Elab[AGeV]: ", Elab, "tsteps: ", tsteps
    do it = 1, tsteps
      read(14,*) Npart
+     print *, "Npart: ", Npart
      read(14,*)
      do i = 1, Npart
        read(14,*) r(0:3), p(0:3), m, ityp, i3
        if (it > nt) then; cycle; endif
-       ! Analysis
+       ! General analysis
        sort = get_sort(ityp)
        Bpart = BfromItyp(ityp)
        Spart = SfromItyp(ityp)
@@ -77,6 +113,18 @@ subroutine Tmn_from_f14(fname)
        total_B(it) = total_B(it) + Bpart
        total_S(it) = total_S(it) + Spart
 
+       jBmu(0:3, it, ix, iz) = jBmu(0:3, it, ix, iz) +&
+                                     upart(0:3) * sf * Bpart
+       jSmu(0:3, it, ix, iz) = jSmu(0:3, it, ix, iz) +&
+                                     upart(0:3) * sf * Spart
+       jmu(0:3, 0, it, ix, iz) = jmu(0:3, 0, it, ix, iz) +&
+                                     upart(0:3) * sf
+       do nu = 0,3
+         Tmn(0:3, nu, 0, it, ix, iz) = Tmn(0:3, nu, 0, it, ix, iz) +&
+                                       upart(0:3) * p(nu) * sf
+       end do
+
+       ! Sort specific analysis
        if (sort < 0) then; cycle; endif
        do ix = 0, nx; do iz = 0, nz ! loop over space grid
          ! dr - comp. frame vector from grid point to particle
@@ -87,14 +135,12 @@ subroutine Tmn_from_f14(fname)
          sf = smearing_factor(dr(1:3), p(0:3))
          upart(0) = 1.0
          upart(1:3) = p(1:3)/p(0)
+         jmu(0:3, sort, it, ix, iz) = jmu(0:3, sort, it, ix, iz) +&
+                                          upart(0:3) * sf
          do nu = 0,3
            Tmn(0:3, nu, sort, it, ix, iz) = Tmn(0:3, nu, sort, it, ix, iz) +&
                                             upart(0:3) * p(nu) * sf
          end do
-         jBmu(0:3, it, ix, iz) = jBmu(0:3, it, ix, iz) +&
-                                       upart(0:3) * sf * Bpart
-         jSmu(0:3, it, ix, iz) = jSmu(0:3, it, ix, iz) +&
-                                       upart(0:3) * sf * Spart
        end do; end do ! end loop over space grid
      end do
    end do
@@ -105,12 +151,15 @@ subroutine Tmn_from_f14(fname)
  Tmn = Tmn / ev
  jBmu = jBmu / ev
  jSmu = jSmu / ev
+ total_p = total_p / ev
+ total_B = total_B / ev
+ total_S = total_S / ev
 
 end subroutine
 
-real function smearing_factor(dr, p)
-  real, intent(in) :: dr(1:3), p(0:3)
-  real gam_inv, bet(1:3), tmp, dr_RF(1:3), dr_RF_sqr
+double precision function smearing_factor(dr, p)
+  double precision, intent(in) :: dr(1:3), p(0:3)
+  double precision gam_inv, bet(1:3), tmp, dr_RF(1:3), dr_RF_sqr
 
   bet(1:3) = p(1:3)/p(0)
   gam_inv = sqrt(1 - bet(1)*bet(1) - bet(2)*bet(2) - bet(3)*bet(3))
@@ -132,33 +181,50 @@ subroutine print_conserved(fname)
   close(8)
 end subroutine
 
+subroutine print_collective_velocities(fname, ix, iz)
+  character(len=*), intent(in) :: fname
+  integer, intent(in) :: ix, iz
+  integer it, sort
+  character(len=12)sname
+  double precision u_hlp(0:3)
+
+  open(unit = 8, file = fname)
+  write(8,'(2(A,f5.1))')"# Collective velocities of particles at x = ",&
+                                                 ix*dx, ", z = ", iz*dz
+  do it = 1, nt
+    write(8,'(A,f5.1,A)')"t = ", it*dt, " fm/c"
+    do sort = 0, max_sort
+      call get_sort_name(sort, sname)
+      u_hlp(0:3) = umu(0:3, sort, it, ix, iz)
+      ! u_mu has lower index, so vector v = -u(1:3)/u(0)
+      write(8,'(3f9.3,A4,A)') -u_hlp(1:3)/u_hlp(0),"  # ",trim(sname)
+    end do
+  end do
+  close(8)
+end subroutine
+
 subroutine print_Tmn(fname, landau, ix, iz)
   character(len=*), intent(in) :: fname
   logical, intent(in) :: landau
   integer, intent(in) :: ix, iz
   integer it, sort, mu, nu
-  real T_hlp(0:3,0:3)
+  double precision T_hlp(0:3,0:3)
+  character(len=12)sname
 
   open(unit = 8, file = fname)
   if (landau) then
-    write(8,*)"# Tmn in Landau rest frame at x = ", ix*dx, ", z = ", iz*dz
+    write(8,'(2(A,f5.1))')"# Tmn in Landau rest frame at x = ",&
+                                          ix*dx, ", z = ", iz*dz
   else
-    write(8,*)"# Tmn in computational frame at x = ", ix*dx, ", z = ", iz*dz
+    write(8,'(2(A,f5.1))')"# Tmn in computational frame at x = ",&
+                                            ix*dx, ", z = ", iz*dz
   endif
 
-  do sort = 1, 7
-    select case(sort)
-      case (1); write(8,*)"# pions"
-      case (2); write(8,*)"# kaons"
-      case (3); write(8,*)"# rhos"
-      case (4); write(8,*)"# nucleons"
-      case (5); write(8,*)"# Deltas"
-      case (6); write(8,*)"# Lambdas"
-      case (7); write(8,*)"# etas"
-      case default; write(8,*)"error: unknown sort ", sort
-    end select
+  do sort = 0, max_sort
+    call get_sort_name(sort, sname)
+    write(8,'(A2,A)')"# ",trim(sname)
     do it = 1, nt
-      write(8,*)"t = ", it*dt
+      write(8,'(A,f5.1,A)')"t = ", it*dt, " fm/c"
       if (landau) then
         T_hlp(0:3,0:3) = TmnL(0:3,0:3, sort, it, ix, iz)
       else
@@ -187,8 +253,25 @@ integer function get_sort(ityp)
   end select
 end function get_sort
 
+subroutine get_sort_name(sort, sname)
+  integer, intent(in) :: sort
+  character(len=12), intent(out) :: sname
+
+  select case(sort)
+    case (0); sname = "total"
+    case (1); sname = "pions"
+    case (2); sname = "kaons"
+    case (3); sname = "rhos"
+    case (4); sname = "nucleons"
+    case (5); sname = "Deltas"
+    case (6); sname = "Lambdas"
+    case (7); sname = "etas"
+    case default; print *,"error: unknown sort ", sort
+  end select
+end subroutine
+
 logical function too_far(dr)
-  real, intent(in) :: dr(1:3)
+  double precision, intent(in) :: dr(1:3)
   if (dr(1)*dr(1) + dr(2)*dr(2) + dr(3)*dr(3) > many_sigma_sqr) then
     too_far = .TRUE.
   else
@@ -204,7 +287,7 @@ logical function read_f14_event_header(uread, Elab, ev, tsteps, dt)
   ! tsteps - number of output moments per event in f14
   ! dt - time difference between output moments
   integer, intent(in) :: uread
-  real, intent(out) :: Elab, dt
+  double precision, intent(out) :: Elab, dt
   integer, intent(out) :: tsteps, ev
 
   character testchar
